e19d3d3e8ce6fd939beae8510aaa8868cff81709
nbllglm <15600992990@163.com>
2020-07-24 19:38:59 +0800

    新增

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,48 @@
+package Main;
+
+
+import Mine.LinkListTwoWay_gz;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        SequenceList_gz sequenceList_gz=new SequenceList_gz<>(10);
+//        sequenceList_gz.add("1");
+//        sequenceList_gz.add("4");
+//        sequenceList_gz.toString();
+//        sequenceList_gz.insert(1,"2");
+//        sequenceList_gz.toString();
+//        sequenceList_gz.insert(2,"3");
+//        sequenceList_gz.toString();
+//        sequenceList_gz.remove(1);
+//        sequenceList_gz.toString();
+
+//        String[] Strings={"1","2","3","4","5","6","7","8"};
+//        String[] Strings={"4","7"};
+//        SeqList seqList=new SeqList(Strings);
+//        Josephus_gz josephus_gz=new Josephus_gz(seqList,"1",3);
+//        josephus_gz.doLogic1();
+//        josephus_gz.doLogic2(seqList.search("1"));
+//
+//        Linklist_gz linklistGz=new Linklist_gz<>();
+//        linklistGz.insert("A");
+//        System.out.println( linklistGz.toString());
+//        linklistGz.insert(1,"B");
+//        linklistGz.insert("C");
+//        System.out.println( linklistGz.toString());
+//        linklistGz.remove(1);
+//        System.out.println( linklistGz.toString());
+//        System.out.println(linklistGz.indexOf("C"));asd
+
+        LinkListTwoWay_gz linkListTwoWay_gz = new LinkListTwoWay_gz();
+        linkListTwoWay_gz.add("A");
+        linkListTwoWay_gz.add("C");
+        linkListTwoWay_gz.add("D");
+        linkListTwoWay_gz.insert(1, "B");
+        linkListTwoWay_gz.insert(0, "*");
+        linkListTwoWay_gz.insert(99, "*");
+        linkListTwoWay_gz.remove(2);
+        linkListTwoWay_gz.indexOf("D");
+
+    }
+}
